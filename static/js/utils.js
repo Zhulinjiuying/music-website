c6a2cb3cfbba6c0b8abb889a182f3d76eb193e6a
Zhulinjiuying <554851075@qq.com>
2017-12-11 15:33:58 +0800

    新增utils.js 修改主页样式

---
+++ static/js/utils.js
@@ -0,0 +1,39 @@
+var log = function() {
+    console.log.apply(console, arguments)
+}
+
+var e = function(selector) {
+    return document.querySelector(selector)
+}
+
+var appendHtml = function(element, html) {
+    element.insertAdjacentHTML('beforeend', html)
+}
+
+var bindEvent = function(element, eventName, callback) {
+    element.addEventListener(eventName, callback)
+}
+
+var toggleClass = function(element, className) {
+    if (element.classList.contains(className)) {
+        element.classList.remove(className)
+    } else {
+        element.classList.add(className)
+    }
+}
+
+var ajax = function(method, path, data, reseponseCallback) {
+    var r = new XMLHttpRequest()
+    // 设置请求方法和请求地址
+    r.open(method, path, true)
+    // 设置发送的数据的格式
+    r.setRequestHeader('Content-Type', 'application/json')
+    // 注册响应函数
+    r.onreadystatechange = function() {
+        if(r.readyState === 4) {
+            reseponseCallback(r)
+        }
+    }
+    // 发送请求
+    r.send(data)
+}
